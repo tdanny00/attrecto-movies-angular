2fa597efc44e374fcd5c29c81d4f75cb3ec0943f
tdanny00 <tolgyesipontdani@gmail.com>
2022-05-29 16:47:06 +0200

    routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { MoviesComponent } from './pages/movies/movies.component';
 import { SandboxComponent } from './pages/sandbox/sandbox.component';
 
 const routes: Routes = [
-  { path: '', component: MoviesComponent},
   { path: 'movies', component: MoviesComponent},
   { path: 'sandbox', component: SandboxComponent},
-  { path: '**', component: SandboxComponent},
+  { path: 'sandbox', redirectTo: '/sandbox', pathMatch:'full' },
+  { path: 'movies', redirectTo: '/movies', pathMatch:'full' },
+  { path: '', redirectTo: '/movies', pathMatch:'full' },
+  { path: '**', component: MoviesComponent},
 
 ];
 
